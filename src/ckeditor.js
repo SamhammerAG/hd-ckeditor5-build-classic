7c600789cd02e691933dbaf37caf98539c30a97d
Christian Kotzbauer <ckotzbauer@samhammer.de>
2018-09-17 08:55:15 +0200

    AID-12338: configured initial feature-set

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -10,16 +10,19 @@ import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
-import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import Image from '@ckeditor/ckeditor5-image/src/image';
-import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+import Table from '@ckeditor/ckeditor5-table/src/table';
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import SimpleUploadImagePlugin from '@samhammer/ckeditor5-simple-image-upload-plugin/src/simple-upload-image-plugin';
 
 export default class ClassicEditor extends ClassicEditorBase {}
@@ -28,18 +31,21 @@ export default class ClassicEditor extends ClassicEditorBase {}
 ClassicEditor.builtinPlugins = [
 	Essentials,
 	SimpleUploadImagePlugin,
+	Alignment,
 	Autoformat,
 	Bold,
 	Italic,
-	BlockQuote,
+	Underline,
+	Strikethrough,
 	Heading,
 	Image,
-	ImageCaption,
 	ImageStyle,
 	ImageToolbar,
 	ImageUpload,
 	Link,
 	List,
+	Table,
+	TableToolbar,
 	Paragraph
 ];
 
@@ -51,22 +57,32 @@ ClassicEditor.defaultConfig = {
 			'|',
 			'bold',
 			'italic',
-			'link',
+			'underline',
+			'strikethrough',
+			'alignment',
+			'|',
 			'bulletedList',
 			'numberedList',
+			'|',
+			'link',
 			'imageUpload',
-			'blockQuote',
+			'insertTable',
+			'|',
 			'undo',
 			'redo'
 		]
 	},
+	alignment: {
+		options: ['left', 'center', 'right', 'justify']
+	},
 	image: {
 		toolbar: [
 			'imageStyle:full',
-			'imageStyle:side',
-			'|',
-			'imageTextAlternative'
+			'imageStyle:side'
 		]
+	},
+	table: {
+		toolbar: ['tableColumn', 'tableRow', 'mergeTableCells']
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
 	language: 'en'
